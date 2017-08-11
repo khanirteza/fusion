0d3291d0ee1cddd67231c25ca53fdadf5d6d11ee
Khan Mohammad Irteza <irteza.khan@gmail.com>
2017-08-11 12:34:36 -0400

    getUserZipCode implemented

--- Fusion/Fusion/UserDataProvider.swift
+++ Fusion/Fusion/UserDataProvider.swift
@@ -14,18 +14,31 @@ import SwiftKeychainWrapper
 class UserDataProvider{
     static var user = [User]()
     static let loggedInUser = KeychainWrapper.standard.string(forKey: currentUser)
+    static var homeZipCode = String()
     
-    static let fetchRequest = NSFetchRequest<User>(entityName: "User")
+    static let userFetchRequest = NSFetchRequest<User>(entityName: "User")
+    
     
     
     public static func getUserPhoto() -> UIImage{
-        fetchRequest.predicate = NSPredicate(format: "emailAddress == %@", loggedInUser!)
+        userFetchRequest.predicate = NSPredicate(format: "emailAddress == %@", loggedInUser!)
         do{
-            user = try CoreDataStack.sharedCoreDataStack.persistentContainer.viewContext.fetch(fetchRequest)
+            user = try CoreDataStack.sharedCoreDataStack.persistentContainer.viewContext.fetch(userFetchRequest)
             return UIImage.init(data: (user.first?.userPhoto)!)!
         } catch let error{
             print(error)
         }
         return #imageLiteral(resourceName: "profile-photo-placeholder")
+    }
+    
+    public static func getUserZipCode() -> String?{
+        userFetchRequest.predicate = NSPredicate(format: "emailAddress == %@", loggedInUser!)
+        do{
+            user = try CoreDataStack.sharedCoreDataStack.persistentContainer.viewContext.fetch(userFetchRequest)
+            return (user.first?.zipCode)!
+        } catch let error{
+            print(error)
+        }
+        return nil
     }
 }
